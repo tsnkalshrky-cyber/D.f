8bd7df87143aa6839eba8683927536abc2884a9e
tsnkalshrky-cyber <tsnkalshrky@gmail.com>
2026-05-09 18:05:04 +0300

    Add files via upload

---
+++ dev.ts
@@ -0,0 +1,6 @@
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/smart-reminder-generator.ts';
+import '@/ai/flows/receipt-transaction-suggester.ts';
+import '@/ai/flows/financial-report-analyzer.ts';
